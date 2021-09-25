a0d7404cdb94d963370bcb8e1ec8dd720cd75974
DavidKorochik <david1korochik@gmail.com>
2021-09-25 18:24:37 +0300

    Finished the Navbar.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from './components/navbar/Navbar';
+import './App.css';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
