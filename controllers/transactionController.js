37cb22e66d2e0e955a45180fe3c0b0e4eed4f573
DavidKorochik <david1korochik@gmail.com>
2021-10-05 13:55:00 +0300

    Finished transactionController and routes

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,4 +2,108 @@ const Transaction = require('../models/Transaction');
 const { validationResult } = require('express-validator');
 require('dotenv').config();
 
-module.exports = {};
+// Create a transaction
+const createTransaction = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array()[0].msg });
+  }
+
+  const { date, description, amount } = req.body;
+
+  try {
+    const transaction = new Transaction({
+      date,
+      description,
+      amount,
+      user: req.user.id,
+    });
+
+    await transaction.save();
+
+    res.json(transaction);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+// Get all transactions
+const getAllTransactions = async (req, res) => {
+  try {
+    const transactions = await Transaction.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    // We don't need to add a condition where we send a message to the UI if there are no transactions because we are handling it in the front
+
+    res.json(transactions);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+// Update a transaction
+const updateTransaction = async (req, res) => {
+  const { date, description, amount } = req.body;
+  const id = req.params.id;
+  const obj = {};
+
+  if (date) obj.date = date;
+  if (description) obj.description = description;
+  if (amount) obj.amouont = amount;
+
+  try {
+    const transaction = await Transaction.findById(id);
+
+    if (!transaction) {
+      return res.status(404).json({ error: 'No transaction was found' });
+    }
+
+    if (transaction.user !== req.user.id) {
+      return res.status(401).json({ error: 'Not authorized' });
+    }
+
+    const updatedTransaction = await Transaction.findByIdAndUpdate(
+      id,
+      {
+        $set: obj,
+      },
+      { new: true }
+    );
+
+    await updatedTransaction.save();
+
+    res.json(updatedTransaction);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+// Delete a transaction
+const deleteTransaction = async (req, res) => {
+  const id = req.params.id;
+  try {
+    let transactionToDelete = await Transaction.findById(id);
+
+    if (!transactionToDelete) {
+      return res.status(404).json({ error: 'No transaction was found' });
+    }
+
+    if (transactionToDelete.user !== req.user.id) {
+      return res.status(401).json({ error: 'Not authorized' });
+    }
+
+    transactionToDelete = await Transaction.findByIdAndDelete(id);
+
+    res.json({});
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  createTransaction,
+  getAllTransactions,
+  updateTransaction,
+  deleteTransaction,
+};
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
+const {
+  createTransaction,
+  getAllTransactions,
+  updateTransaction,
+  deleteTransaction,
+} = require('../controllers/transactionController');
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('date', 'Date is rqeuired').not().isEmpty(),
+      check('description', 'Description is required').not().isEmpty(),
+      check('amount', 'Amount is required').not().isEmpty(),
+    ],
+  ],
+  createTransaction
+);
+
+router.get('/', auth, getAllTransactions);
+
+router.put('/:id', auth, updateTransaction);
+
+router.delete('/:id', auth, deleteTransaction);
 
 module.exports = router;
