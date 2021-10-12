fe533e152466400bb057a3161a15b1393a703ccc
DavidKorochik <david1korochik@gmail.com>
2021-10-12 17:46:41 +0300

    Added validation to the User model

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const transactionsSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'users',
+    ref: 'User',
   },
   date: {
     type: String,
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
-  name: {
+  fullName: {
     type: String,
     required: true,
   },
