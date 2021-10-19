eba501334f7ba96df3aae2c7bf36283801648637
DavidKorochik <david1korochik@gmail.com>
2021-10-19 20:58:32 +0300

    Changes the title and description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,11 @@ import React, { Fragment } from 'react';
 export default function About() {
   return (
     <Fragment>
+      <div className='text-white font-bold text-6xl flex justify-center mt-20 mb-14'>
+        <h1>
+          About <span className='text-blue-500 font-bold'>Us</span>
+        </h1>
+      </div>
       <div className='text-4xl flex justify-center mt-28 leading-relaxed text-white'>
         Our goal is to help manage your monthly budget so you can keep an eye on
         <br />
