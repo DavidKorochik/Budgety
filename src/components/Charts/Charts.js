76cdc74237d2505f5d0ba037a22a256bd666e924
DavidKorochik <david1korochik@gmail.com>
2021-10-19 20:59:24 +0300

    Added transition to the charts and a spinner

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,31 +1,37 @@
-import React, { Fragment, useContext, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
-import { useHistory } from 'react-router-dom';
+import React, { Fragment, useContext, useState, useEffect } from 'react';
+import { Pie, Bar } from 'react-chartjs-2';
 import { TransactionsContext } from '../../store/transactions/TransactionsState';
 import Spinner from '../../utils/Spinner';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../utils/animations';
 import './Charts.css';
 
 export default function Charts() {
   const { transactions, getAllTransactions, loading } =
     useContext(TransactionsContext);
 
-  let history = useHistory();
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(async () => {
     setTimeout(async () => {
       await getAllTransactions();
-    }, 2000);
+      setIsVisible(!isVisible);
+    }, 1500);
   }, []);
 
-  // const getMonthName = (month) => {
-  //   const d = new Date();
-  //   d.setMonth(month - 1);
-  //   const monthName = d.toLocaleString('default', { month: 'long' });
-  //   return monthName;
-  // };
+  const getMonthName = (month) => {
+    const d = new Date();
+    d.setMonth(month - 1);
+    const monthName = d.toLocaleString('default', { month: 'long' });
+    return monthName;
+  };
+
+  //
+
+  //
 
   const amounts = transactions.map((t) => Number(t.amount));
-  // const dates = transactions.map((t) => getMonthName(t.date.split('-')[1]));
+  const dates = transactions.map((t) => getMonthName(t.date.split('-')[1]));
 
   let incomes = 0;
   let expanses = 0;
@@ -55,42 +61,59 @@ export default function Charts() {
       {loading ? (
         <Spinner />
       ) : (
-        <div className='flex justify-center charts ml-40 mt-16 items-center w-1/4'>
-          {incomes === 0 && expanses === 0 ? (
-            <h1 className='text-blue-500 text-4xl'>
-              You need Expnases or Incomes for{' '}
-              <span className='font-extrabold'>
-                "Expanses VS Incomes" Chart
-              </span>
-              !
-            </h1>
+        <Fragment>
+          {!isVisible ? (
+            <Spinner />
           ) : (
-            <Fragment>
-              <Pie
-                data={state}
-                options={{
-                  plugins: {
-                    title: {
-                      display: true,
-                      color: 'white',
-                      font: { weight: 'bold', size: 24 },
-                      text: 'Expanses VS Incomes Chart',
-                    },
-                    legend: {
-                      labels: {
-                        font: {
-                          size: 14,
+            <motion.div
+              variants={fadeIn}
+              animate={isVisible ? 'show' : ''}
+              initial='hidden'
+              className='flex justify-center charts ml-40 mt-16 items-center w-1/4'
+            >
+              {incomes === 0 && expanses === 0 ? (
+                <h1 className='text-blue-500 text-4xl'>
+                  You need Expnases or Incomes for{' '}
+                  <span className='font-extrabold'>
+                    "Expanses VS Incomes" Chart
+                  </span>
+                  !
+                </h1>
+              ) : (
+                <Fragment>
+                  <motion.div
+                    variants={fadeIn}
+                    animate={isVisible ? 'show' : ''}
+                    initial='hidden'
+                  >
+                    <Pie
+                      data={state}
+                      options={{
+                        plugins: {
+                          title: {
+                            display: true,
+                            color: 'white',
+                            font: { weight: 'bold', size: 24 },
+                            text: 'Expanses VS Incomes Chart',
+                          },
+                          legend: {
+                            labels: {
+                              font: {
+                                size: 14,
+                              },
+                              color: 'white',
+                              padding: 10,
+                            },
+                          },
                         },
-                        color: 'white',
-                        padding: 12,
-                      },
-                    },
-                  },
-                }}
-              />
-            </Fragment>
+                      }}
+                    />
+                  </motion.div>
+                </Fragment>
+              )}
+            </motion.div>
           )}
-        </div>
+        </Fragment>
       )}
     </Fragment>
   );
